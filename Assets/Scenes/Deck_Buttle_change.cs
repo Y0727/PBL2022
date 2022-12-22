ce257cc13acaca33e2d8074efc5a004cd24876d9
IbukiNagakubo <ibukipythen2020@icloud.com>
2022-12-22 18:22:34 +0900

    Create：デッキ画面から対戦画面への画面遷移スクリプトの作成

---
+++ Assets/Scenes/Deck_Buttle_change.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Deck_Buttle_change : MonoBehaviour
+{
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadScene("VS_Ryu");
+    }
+}
+
